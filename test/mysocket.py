b8bc43762a07d2b101076b9e42ca28433fcc53dc
Hirata Yuya <2021d35@stu.gifu-nct.ac.jp>
2024-10-18 16:25:36 +0900

    説明増やした

--- test/mysocket.py
+++ test/mysocket.py
@@ -1,7 +1,7 @@
-import socket
+import socket   #ソケット通信
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+import time    #時間
 
 z = np.arange(0, 20, 0.5)
 x = np.cos(z)
@@ -14,10 +14,8 @@ if __name__ == '__main__':
     socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #オブジェクトの作成
     socket_client.connect((host, port))                               #サーバに接続
 
-    #socket_client.send('送信するメッセージ')                #データを送信 Python2
-    #socket_client.send(k.encode('utf-8')) #データを送信 Python3
-    
-    #socket_client.send(l.encode('utf-8')) #データを送信 Python3
+    #socket_client.send('送信するメッセージ') ←これでprocessing側に送信(0.07秒くらい遅らせてほしい)
+#ここからはprocessing動作確認様用なので考えなくていいです
 for i in np.arange(0.0,20.0,0.5):
     #print("FFFF," + str(i*10) + "," + str(round(np.sin(i),3)*100 + 150) + "," + str(round(np.cos(i),3)*100 + 150) + "," + str((i+0.5)*10) + "," + str(round(np.sin(i+0.5),3)*100 + 150) + "," + str(round(np.cos(i+0.5),3)*100 + 150))
     #socket_client.send(("FFFF," + str(i*10) + "," + str(round(np.sin(i),3)*100 + 150) + "," + str(round(np.cos(i),3)*100 + 150) + "," + str((i+0.5)*10) + "," + str(round(np.sin(i+0.5),3)*100 + 150) + "," + str(round(np.cos(i+0.5),3)*100 + 150)).encode('utf-8'))
@@ -28,7 +26,7 @@ for i in np.arange(0.0,20.0,0.5):
 #socket_client.send(("1111," + str(200) + "," + str(round(np.sin(20),3)*100 + 150) + "," + str(round(np.cos(20),3)*100 + 150) + "," + str((20+0.5)*10) + "," + str(round(np.sin(20+0.5),3)*100 + 150) + "," + str(round(np.cos(20+0.5),3)*100 + 150)).encode('utf-8'))
 socket_client.send(("FFFF," + str(200) + "," + str(round(np.sin(20),3)*100 + 150) + "," + str(round(np.cos(20),3)*100 + 150)).encode('utf-8'))
 time.sleep(0.07)
-socket_client.send(("AAAA," + str(1) + "," + str(1) + "," + str(1)).encode('utf-8'))
+socket_client.send(("AAAA," + str(1) + "," + str(1) + "," + str(1)).encode('utf-8'))  #AAAAの後は適当な数字でいい
 time.sleep(0.07)
 for i in np.arange(0.0, 20.0, 0.5):
     print("FFFF," + str(i*10) + "," + str(round(np.cos(i),3)*100 + 150) + "," + str(round(np.sin(i),3)*100 + 150))
